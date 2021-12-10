54b984fafc350739584056d37c3921c5787ce390
GlendleNguyen <glendle.nguyen@gmail.com>
2021-12-10 21:38:47 +0800

    .

--- scripts.js
+++ scripts.js
@@ -1,15 +1,7 @@
-const DEFAULT_SIZE = 16
-
-const board = document.getElementById('board')
-
-function createGrid(size) {
-    for(let i = 0; i < size; i++) {
-        const gridCell = document.createElement('div')
-        board.appendChild(gridCell)
-    }
+function helloWorld() {
+    console.log("hello world!");
 }
 
-function hello() {
-    console.log('Hello!')
-    return 1
-}
+
+
+
